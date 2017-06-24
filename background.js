df119b763d7f3d7cde308331684681949eb1f44d
Snipower <thereceiverofjunkmail@gmail.com>
2017-06-23 23:13:21 -0500

    Toggleable icon

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
-chrome.browserAction.onClicked.addListener(function(tab) {});
+var toggle=true;
+function update(){
+  if(toggle){
+    //disable
+    chrome.browserAction.setIcon({path:"fadedicon.png"});
+  }else{
+    //enable
+    chrome.browserAction.setIcon({path:"icon.png"});
+  }
+  toggle=!toggle;
+}
+chrome.browserAction.onClicked.addListener(update);
