63a30618dd8ed4a80aba3669fc124862fc74da95
Snipower <thereceiverofjunkmail@gmail.com>
2017-06-24 16:03:16 -0500

    Toggleable button turns likes on/off

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
-var toggle=true;
+var toggle=false;
 function update(){
+  
   if(toggle){
-    //disable
-    chrome.browserAction.setIcon({path:"fadedicon.png"});
-  }else{
-    //enable
+    //disable likes
+    chrome.tabs.executeScript((null, {file: "disable.js"}));
     chrome.browserAction.setIcon({path:"icon.png"});
+    chrome.browserAction.setTitle({title:"Enable likes"});
+  }else{
+    //enable likes
+    chrome.tabs.executeScript((null, {file: "enable.js"}));
+    chrome.browserAction.setIcon({path:"fadedicon.png"});
+    chrome.browserAction.setTitle({title:"Disable likes"});
   }
   toggle=!toggle;
 }
---
+++ disable.js
@@ -0,0 +1,4 @@
+var likes=document.getElementsByClassName("UFILikeLink");
+for(var i=0;i<likes.length;i++){
+  likes[i].style.cssText = 'display:none !important';
+}
